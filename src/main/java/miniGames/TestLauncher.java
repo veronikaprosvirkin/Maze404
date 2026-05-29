4091dcbc9a039ad467377914f45e267165ef92f6
Veronika <veronika.prosvirkin@gmail.com>
2026-05-29 21:03:50 +0300

    feat(miniGames): add TestLauncher and TestMiniGames for game testing interface

---
+++ src/main/java/miniGames/TestLauncher.java
@@ -0,0 +1,7 @@
+package miniGames;
+
+public class TestLauncher {
+    public static void main(String[] args) {
+        TestMiniGames.main(args);
+    }
+}
---
+++ src/main/java/miniGames/TestMiniGames.java
@@ -0,0 +1,31 @@
+package miniGames;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TestMiniGames extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button guessGameBtn = new Button("Test: Guess The Number");
+        guessGameBtn.setOnAction(e -> GuessTheNumber.startNewGame());
+
+        Button clickerGameBtn = new Button("Test: Clicker Game");
+        clickerGameBtn.setOnAction(e -> ClickerMiniGame.startNewGame());
+
+        VBox root = new VBox(20, guessGameBtn, clickerGameBtn);
+        root.setStyle("-fx-padding: 50px; -fx-alignment: center;");
+
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Mini Games Test");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
