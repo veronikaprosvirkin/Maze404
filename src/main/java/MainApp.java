f520f8dfd5a3ae693ec9fc1181ac555eae85035a
mykhailopulov <mihail.pulov.247@gmail.com>
2026-06-01 14:31:32 +0300

    feat(GamePanel): mist adapts to different difficulties

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -57,7 +57,7 @@ public class MainApp extends Application {
 
         GamePanel gamePanel = new GamePanel(grid, player, Difficulty.current);
         boolean mistEnabled = true;
-        double mistAnimationTime = 3.0;
+        double mistAnimationTime = 2.0;
         gamePanel.setMistEnabled(mistEnabled);
         gamePanel.setMistAnimationTimeScale(mistAnimationTime);
         double baseWidth = grid.getWidth() * 32.0;
