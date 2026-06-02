76c64bccadfae28a2fcd1558a6235ecfe6b39d14
Veronika <veronika.prosvirkin@gmail.com>
2026-06-02 18:56:22 +0300

    feat(EventBus): implement event publishing and subscription methods

--- src/main/java/events/EventBus.java
+++ src/main/java/events/EventBus.java
@@ -1,5 +1,6 @@
 package events;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
@@ -14,5 +15,18 @@ public class EventBus {
     public static EventBus getInstance() { return INSTANCE; }
 
     public void publish(GameEvent gameEvent) {
+        GameEvent.Type type = gameEvent.getType();
+        List<Consumer<GameEvent>> eventListeners = listeners.get(type);
+
+        if (eventListeners != null) {
+            for (Consumer<GameEvent> listener : eventListeners) {
+                listener.accept(gameEvent);
+            }
+        }
+    }
+
+    public void subscribe(GameEvent.Type type, Consumer<GameEvent> listener) {
+        listeners.computeIfAbsent(type, k -> new ArrayList<>());
+        listeners.get(type).add(listener);
     }
 }
