dd103f1024f22285527e1214f106e98035c597ea
Anastasiia Shapoval <nastya030207@gmail.com>
2026-06-04 10:51:51 +0300

    feat: add SaveManager and SaveData for game progress persistence

---
+++ src/main/java/model/SaveData.java
@@ -0,0 +1,9 @@
+package model;
+
+public class SaveData {
+    public int     highScoreLevel1 = 0;
+    public int     highScoreLevel2 = 0;
+    public int     highScoreLevel3 = 0;
+    public int     unlockedLevel   = 1;
+    public boolean soundEnabled    = true;
+}
