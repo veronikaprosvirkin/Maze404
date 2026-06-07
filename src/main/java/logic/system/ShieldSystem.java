523808cdff27e096a179f3424e43638d9f255fb7
mykhailopulov <mihail.pulov.247@gmail.com>
2026-06-08 00:15:24 +0300

    feat(MainApp): fix inventory bar height

--- src/main/java/logic/system/ShieldSystem.java
+++ src/main/java/logic/system/ShieldSystem.java
@@ -9,8 +9,11 @@ import model.Position;
 public class ShieldSystem {
     public void activateShield(GameState state) {
         Player player = state.getPlayer();
-        if(player.hasShield())
+        if (player.hasShield() || player.getShieldCount() <= 0) {
             return;
+        }
+
+        player.addShield(-1);
         player.activateShield();
         EventBus.getInstance().publish(new GameEvent(GameEvent.Type.SHIELD_ACTIVATED,
                 new Position(player.getRow(), player.getCol())));
