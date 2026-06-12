1e7b0e1990e5630826a6d9a7fdf1e8fdf65934f3
mykhailopulov <mihail.pulov.247@gmail.com>
2026-06-12 23:53:12 +0300

    feat(LevelIsland): enhance mini-game artifact collection and prompt handling

--- src/main/java/miniGames/MiniGameManager.java
+++ src/main/java/miniGames/MiniGameManager.java
@@ -1,13 +1,10 @@
 package miniGames;
 
 import enums.Difficulty;
-import enums.ArtifactType;
-import model.Artifact;
 import model.GameState;
 import events.EventBus;
 import events.GameEvent;
 import model.Player;
-import model.Position;
 import ui.render.LevelIsland;
 
 import java.util.ArrayList;
@@ -22,9 +19,7 @@ public class MiniGameManager {
     private final Random random = new Random();
     private final GameState gameState;
     private final Consumer<GameEvent> miniGameTriggerListener;
-    private final Consumer<GameEvent> playerMovedListener;
     private final List<Integer> availableGames = new ArrayList<>();
-    private Position promptPosition;
     private boolean promptActive = false;
 
     public MiniGameManager(GameState gameState, Player player, LevelIsland levelIsland) {
@@ -33,20 +28,13 @@ public class MiniGameManager {
         this.levelIsland = levelIsland;
 
         miniGameTriggerListener = event -> {
-            askToPlay(event.getPosition());
-        };
-        playerMovedListener = event -> {
-            if (promptActive && promptPosition != null && !promptPosition.equals(event.getPosition())) {
-                rejectPrompt();
-            }
+            askToPlay();
         };
         EventBus.getInstance().subscribe(GameEvent.Type.MINI_GAME_TRIGGERED, miniGameTriggerListener);
-        EventBus.getInstance().subscribe(GameEvent.Type.PLAYER_MOVED, playerMovedListener);
     }
 
     public void dispose() {
         EventBus.getInstance().unsubscribe(GameEvent.Type.MINI_GAME_TRIGGERED, miniGameTriggerListener);
-        EventBus.getInstance().unsubscribe(GameEvent.Type.PLAYER_MOVED, playerMovedListener);
     }
 
     public boolean handlePromptKey(javafx.scene.input.KeyCode keyCode) {
@@ -67,9 +55,9 @@ public class MiniGameManager {
         return false;
     }
 
-    private void askToPlay(Position position) {
-        promptPosition = position;
+    private void askToPlay() {
         promptActive = true;
+        gameState.setPaused(true);
         levelIsland.showChoiceMessage(
                 enums.ArtifactType.MINI_GAME,
                 "Enter the challenge?",
@@ -87,22 +75,15 @@ public class MiniGameManager {
         }
 
         promptActive = false;
-        Position acceptedPosition = promptPosition;
-        promptPosition = null;
         levelIsland.hideChoiceMessage();
 
-        if (!hasMiniGameArtifactAt(acceptedPosition)) {
-            return;
-        }
-
         if (player.useCrystal()) {
-            collectMiniGameArtifactAt(acceptedPosition);
-            gameState.setPaused(true);
             launchRandomMiniGame();
             gameState.setPaused(false);
             return;
         }
 
+        gameState.setPaused(false);
         levelIsland.showArtifactMessage(enums.ArtifactType.CRYSTAL, "Need 1 crystal");
     }
 
@@ -112,34 +93,8 @@ public class MiniGameManager {
         }
 
         promptActive = false;
-        promptPosition = null;
+        gameState.setPaused(false);
         levelIsland.hideChoiceMessage();
-    }
-
-    private boolean hasMiniGameArtifactAt(Position position) {
-        if (position == null) {
-            return false;
-        }
-
-        return gameState.getArtifacts().stream()
-                .anyMatch(artifact -> isAvailableMiniGameArtifactAt(artifact, position));
-    }
-
-    private void collectMiniGameArtifactAt(Position position) {
-        gameState.getArtifacts().stream()
-                .filter(artifact -> isAvailableMiniGameArtifactAt(artifact, position))
-                .findFirst()
-                .ifPresent(artifact -> {
-                    artifact.collect();
-                    EventBus.getInstance().publish(new GameEvent(GameEvent.Type.ARTIFACT_COLLECTED, position));
-                });
-    }
-
-    private boolean isAvailableMiniGameArtifactAt(Artifact artifact, Position position) {
-        return artifact != null
-                && !artifact.isCollected()
-                && artifact.getType() == ArtifactType.MINI_GAME
-                && position.equals(artifact.getPosition());
     }
 
     private void launchRandomMiniGame() {
